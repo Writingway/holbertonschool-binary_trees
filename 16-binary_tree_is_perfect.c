9977ef5f558a7f9d310d9315b3ead8f19d2949fb
Panaki-GILLOT <gillotpanaki@gmail.com>
2025-12-10 11:25:02 +0100

    feat: add binary_tree_is_perfect function to check

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -36,7 +36,7 @@ static size_t binary_tree_size(const binary_tree_t *tree)
 * binary_tree_is_perfect - checks if a binary tree is perfect
 * @tree: pointer to the root node
 *
-* Return: 1 if perfect, 0 otherwise
+* Return: 1 if perfect, 0 otherwise.
 */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
