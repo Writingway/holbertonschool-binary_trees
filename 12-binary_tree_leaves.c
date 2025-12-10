9735ce067f2813812ae26ba1478eb3efaf0f350d
Panaki-GILLOT <gillotpanaki@gmail.com>
2025-12-10 10:45:57 +0100

    style: fix comment formatting in binary_tree_leaves and binary_trees.h

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -4,7 +4,7 @@
 * binary_tree_leaves - counts the leaves in a binary tree
 * @tree: pointer to the root node of the tree
 *
-* Return: number of leaves, 0 if tree is NULL
+* Return: number of leaves, 0 if tree is NULL.
 */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
